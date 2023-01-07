bd1f941631841e6fe413af0f1d799b25de9b51d4
lokeshsingh09 <lokeshrajput0009@gmail.com>
2023-01-07 22:41:01 +0530

    about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const About=()=> {
+    const myStyle={ 
+        width:'300px', 
+        height:'300px',
+    };
     return (
         <div className="container">
         <div className="py-4">
         <h1>About Page</h1>
-    <p className="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere facilis consectetur culpa aut perspiciatis, voluptatum recusandae reiciendis omnis adipisci eos sed porro! Dignissimos aliquid dolor dolore velit sed quos eum saepe sequi voluptatem, labore illo accusamus, iusto excepturi similique laboriosam.</p>
-    <p className="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere facilis consectetur culpa aut perspiciatis, voluptatum recusandae reiciendis omnis adipisci eos sed porro! Dignissimos aliquid dolor dolore velit sed quos eum saepe sequi voluptatem, labore illo accusamus, iusto excepturi similique laboriosam.</p>
-    <p className="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere facilis consectetur culpa aut perspiciatis, voluptatum recusandae reiciendis omnis adipisci eos sed porro! Dignissimos aliquid dolor dolore velit sed quos eum saepe sequi voluptatem, labore illo accusamus, iusto excepturi similique laboriosam.</p>
-
+    
+    <img style={myStyle} src="../lokesh.jpg" alt=""/>
+    <h6>Created By<h4>Lokesh Rajput</h4> </h6>
         </div>
         </div>
     )
